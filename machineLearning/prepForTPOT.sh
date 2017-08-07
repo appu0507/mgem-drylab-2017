80effe8b8fb5909c0e4efb365d11a9afe352f322
Sid Reed <sidreed34@gmail.com>
2017-08-07 00:21:13 -0400

    a short bash script than converts a fasta file into a csv and the sequence into a binary code, so tpot can be used on it

---
+++ machineLearning/prepForTPOT.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# $1 is a dereplicated fasta file, $2 it the organism and round number
+python ../convertReadsToCSV.py $1 opt.tmp
+cat opt.tmp | tr 'A' '00'| tr 'G' '01'| tr 'C' '10'| tr 'T' '11' > "$2""Binary.csv"
+rm opt.tmp
