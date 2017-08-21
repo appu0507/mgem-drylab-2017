6151eb2380f4efbee03609d14171a7df2699cb96
Sid Reed <sidreed34@gmail.com>
2017-08-21 00:05:27 -0400

    changes it so that the reads are expressed as a proportion of the maximum fitness score, not the raw scores

--- machineLearning/convertReadsToCSV.py
+++ machineLearning/convertReadsToCSV.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import division
 import sys, csv, pysam
 '''
 Convert this into a csv where records are [header, reads, sequence]
@@ -15,6 +16,7 @@ fasta_file = pysam.FastaFile(sys.argv[1])
 with open(sys.argv[2], 'wb') as csvf:
     fasta_writer = csv.writer(csvf, delimiter=',')
     fasta_writer.writerow(['reads', 'sequence'])
+    maxfit = max([int(str(ref).split('=')[1].strip('\n;')) for ref in fasta_file.references])
     for ref in fasta_file.references:
-        fasta_writer.writerow([(str(ref).split('=')[1]).strip('\n;'), fasta_file.fetch(ref)])
+        fasta_writer.writerow([int((str(ref).split('=')[1]).strip('\n;'))/maxfit, fasta_file.fetch(ref)])
 
