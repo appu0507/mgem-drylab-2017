2bef7ff7e760eece8077bdc82482a84c9ba00dcd
Sid Reed <sidreed34@gmail.com>
2017-08-21 01:06:59 -0400

    moved stuff to depreciated, either its functionality was integrated into other stuff or it is not useful

--- machineLearning/prepForTPOT_int.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $1 is a dereplicated fasta file, $2 it the organism and round number
-# This version converts bases to 3 bits; need '000' to pad length??
-python ./convertReadsToCSV.py $1 opt.tmp
-cat opt.tmp | sed 's/A/1/g'| sed 's/G/2/g'| sed 's/C/3/g'| sed 's/T/4/g' > "$2""int.csv"
-rm opt.tmp
