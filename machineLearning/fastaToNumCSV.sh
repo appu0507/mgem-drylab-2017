0bc9265cc78a6fd5f68c113620e94b2e77ed310f
Sid Reed <sidreed34@gmail.com>
2017-08-22 01:26:28 -0400

    added flag option to split seq into kmers of a specified size by splitting it up with , in csv format

--- machineLearning/fastaToNumCSV.sh
+++ machineLearning/fastaToNumCSV.sh
@@ -14,9 +14,10 @@ if [ $# -lt 3 ]
 then 
         cat opt.tmp | sed 's/A/1/g'| sed 's/G/2/g'| sed 's/C/3/g'| sed 's/T/4/g' > "${@: -1}""int.csv"
 else
-    while getopts 'b' opt ; do
+    while getopts 'k:b' opt ; do
         case $opt in
-             b) cat opt.tmp | tr 'A' '00'| tr 'G' '01'| tr 'C' '10'| tr 'T' '11' > "${@: -1}""Binary.csv" ;;
+            k) cat opt.tmp |  perl -pe "s/.{$OPTARG}(?=.*,)/$&,/g" | sed 's/,,/,/g' | sed 's/,\./\./g' | sed 's/\.,/\./g' | sed 's/A/1/g'| sed 's/G/2/g'| sed 's/C/3/g'| sed 's/T/4/g' > "${@: -1}""int.csv" ;;
+            b) cat opt.tmp | tr 'A' '00'| tr 'G' '01'| tr 'C' '10'| tr 'T' '11' > "${@: -1}""Binary.csv" ;;
         esac
     done
 fi   
