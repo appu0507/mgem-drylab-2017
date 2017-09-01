693ec6a7681a150d53c027193f6bc91f1d43bc22
Sid Reed <sidreed34@gmail.com>
2017-08-31 20:11:15 -0400

    vizualizing shape of data to fix errors in regressionNet.py

--- machineLearning/testData/pdtest.py
+++ machineLearning/testData/pdtest.py
@@ -2,12 +2,32 @@
 import sys
 import numpy as np
 import pandas as pd
-import matplotlib.pylab as plt
 # load readscsv data
-dataframe = pd.read_csv(str(sys.argv[1]), delimiter=",", header=1)
+dataframe = pd.read_csv(str(sys.argv[1]), delimiter=",")
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
 seqs = dataset[:,0]
 reads = dataset[:,1]
-print seqs 
-print reads
+seqs = [str(int(s)) for s in seqs]
+
+print(seqs)
+print("seqs len:" + str(len(seqs)))
+print("reads len:" + str(len(reads)))
+
+#pad seqs to be same len
+max_length = 0
+for seq in seqs:
+    if len(seq) > max_length:
+        max_length = len(seq)
+
+#max_length = max_length + (3-(max_length%3))
+print("maxlen is:" + str(max_length))
+
+for i,seq in enumerate(seqs):
+    diff = max_length - len(seq)
+    padded_sequence = seq + diff*'0'
+    padded_sequence_list = list(padded_sequence)
+    padded_sequence_list_ints = [ int(x) for x in padded_sequence_list ]
+    seqs[i] = [padded_sequence_list_ints]
+
+print(seqs[1:10])
