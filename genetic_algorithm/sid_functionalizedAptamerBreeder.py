ecb399adea82355137d0c7bd7e49d2ce913b1e72
Sid Reed <sidreed34@gmail.com>
2017-09-10 05:42:33 -0400

    changed getfitness to be compatible with ML model, updated function calls to it

--- genetic_algorithm/sid_functionalizedAptamerBreeder.py
+++ genetic_algorithm/sid_functionalizedAptamerBreeder.py
@@ -7,6 +7,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Embedding
 from keras.layers.recurrent import LSTM
 from sklearn.model_selection import train_test_split
+
 #made by sid for gluedtogether.py
 def getAptamers(readfile):
     with open(readfile,"r") as input_sequences:
@@ -39,40 +40,37 @@ def generateAptamer(length=20): # function to generate a random aptamer length n
 #      else:
 #         aptamer += "T"
 
-def getFitness(mdl, sequence):
-    score = mdl.predict(sequence)
-    return score
+#returns predicted fitness sequence of a model
+def getFitness(seqi, mdl):
+    tmp = [1 if bp == 'A' else bp for bp in seqi ]
+    tmp = [2 if bp == 'C' else bp for bp in tmp ]
+    tmp = [3 if bp == 'G' else bp for bp in tmp ]
+    tmp = [4 if bp == 'T' else bp for bp in tmp ]
+    tmp = ''.join(str(e) for e in tmp)
+    df = pd.DataFrame()
+    df.set_value(0,0,tmp)
+    seqs = df.values
+    seqs = [str(int(seq)) for seq in seqs[:,0]]
+
+    max_length = 286 #this was the max length from training 
+
+    for i,seq in enumerate(seqs):
+        diff = max_length - len(seq)
+        padded_sequence = seq + diff*'0'
+        padded_sequence_list = list(padded_sequence)
+        padded_sequence_list_ints = [ int(x) for x in padded_sequence_list ]
+        seqs[i] = [padded_sequence_list_ints]
+
+    return float(mdl.predict(np.array(seqs)))
+
 #DEPRECIATED
 # generates a random pool of aptamers using the generateAptamer function
-def genPool(pool_size, apt_size=20, mdl):
+def genPool(pool_size, mdl, apt_size=20):
      aptamerList = []
      for i in range(1,pool_size):
          seq = generateAptamer(apt_size)
          aptamerList.append(['aptamer_' + str(i), seq, getFitness(mdl,seq)])
      return sorted(aptamerList, key=lambda x: x[2], reverse=True) 
-
-
-
-##uses difflib string distance to compute child fitness from parents
-## parent1 and parent 2 are in the format [aptamer_1, 'asdasdasdasdasda', 47]
-## child is just a string, 'asdasdasdasdasdasdasd'
-#def computeChildFitness(parent1=None, parent2=None, child=None):
-#    #take distance to parent1, divide by len(child), then multiply by parent1 fitness. repeat for parent 2 and add the fitness values. ???????????????
-#    if parent1 == None and parent2 == None:
-#        raise ValueError('Need at least on valid parent')
-#    elif parent1 == None:
-#        return parent2[2]
-#    elif parent2 == None:
-#        return parent1[2]
-#    elif parent1 != None and parent2 != None and child != None:
-#        #TODO compute child fintess
-#        # fitness function can be based on parents, needs to be only based on the sequence
-#        # THIS IS A MADE UP PLACE HOLDER IT IS NOT INTENDED FOR FINAL USE
-#        # return mean([parent1[2], parent2[2]])*mean([SequenceMatcher(None, parent1[1], child).ratio(), SequenceMatcher(None, parent2[1], child).ratio()])
-#        return  
-#    else:
-#        raise ValueError('Invalid paramaters, need parent1, parent2 and child to all not be None')
-
 
 
 # parent1 and parent 2 are in the format [aptamer_1, 'asdasdasdasdasda', 47]
@@ -82,10 +80,10 @@ def crossover(parent1, parent2, idnum, gennum, mdl):
     if crossOverPos%2 == 0:
         # if crossOverpos is even, first half of the child is from parent1, if not first half of child is from parent2
         childseq = parent1[1][:crossOverPos] + parent2[1][crossOverPos:]
-        child = ["gen_" + str(gennum) + "_offspring_" + str(idnum), childseq, getFitness(mdl, childseq)]
+        child = ["gen_" + str(gennum) + "_offspring_" + str(idnum), childseq, getFitness(childseq, mdl)]
     else:
         childseq = parent2[1][:crossOverPos] + parent1[1][crossOverPos:]
-        child = ["gen_" + str(gennum) + "_offspring_" + str(idnum), childseq, getFitness(mdl, childseq)]
+        child = ["gen_" + str(gennum) + "_offspring_" + str(idnum), childseq, getFitness(childseq, mdl)]
     return child
 #   if crossOverPos == 0:
 #      # just returns parent 1 since not acutally a crossover
@@ -99,7 +97,7 @@ def crossover(parent1, parent2, idnum, gennum, mdl):
 #aptamer list format: [['aptamer_1, 'asdasdasdasd', 45], ['aptamer_2', 'asdasasdasd', 78]]
 #the two highest scoring aptamers are randomly crossed over to generate a specified number of offspring
 #assumed all aptamers are the same length
-def breed(aptamers, gennum, top_cutoff=0.10, mdl):
+def breed(aptamers, gennum, mdl, top_cutoff=0.10):
     # sortedAptamerList should already be sorted but just because im paranoid im going to sort it again
     sortedAptamerList = sorted(aptamers, key=lambda x: x[2], reverse=True) 
     # list initialization
