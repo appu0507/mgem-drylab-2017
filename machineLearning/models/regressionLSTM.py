efcf8378b912ca164d0004faca0cd899f8577347
Sid Reed <sidreed34@gmail.com>
2017-10-19 05:41:57 -0400

    nothing major, may have moved some lines around when debuging but works the same

--- machineLearning/models/regressionLSTM.py
+++ machineLearning/models/regressionLSTM.py
@@ -2,6 +2,10 @@ from __future__ import division
 import sys
 import numpy as np
 import pandas as pd
+
+seed = 9
+np.random.seed(seed)
+
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Embedding
 from keras.layers.recurrent import LSTM
@@ -15,17 +19,17 @@ if len(sys.argv) < 6:
     print("arg3 is number of epochs")
     print("arg4 is the filepath where the model willbe saved to (*.h5)")
     print("arg5 is the filepath to the plot of the prediction results (*.png)")
-    print("arg6 is the number of datapts that will be graphed")
-    
+    print("arg6 is the proportion of data to be used for validation, not learning")
+
+
+
 # fix random seed for reproducibility
-seed = 9
-np.random.seed(seed)
-# load readscsv data
+# load tempscsv data
 dataframe = pd.read_csv(str(sys.argv[1]), delimiter=",")
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
 seqs = dataset[:,0]
-reads = dataset[:,1]
+temps = dataset[:,1]
 seqs = [str(int(seq)) for seq in seqs]
 #pad seqs to be same len
 max_length = 0
@@ -45,7 +49,7 @@ for i,seq in enumerate(seqs):
 
 
 #split into test and trian data
-(seqstrain, seqstest, readstrain, readstest) = train_test_split(seqs, reads, test_size=0.04, random_state=seed)
+(seqstrain, seqstest, tempstrain, tempstest) = train_test_split(seqs, temps, test_size=float(sys.argv[6]), random_state=seed)
 #TRAIN IT ON ALL THE DATA VALIDATE WITH OTHER DATA
 #creating model
 model = Sequential()
@@ -57,32 +61,35 @@ model.add(Dropout(0.2))
 model.add(LSTM(10, input_shape=(20,1), return_sequences=False))  
 model.add(Dropout(0.2))
 model.add(Dense(units=1, input_shape=(10,1))) 
-
 model.add(Activation("sigmoid"))  
+
 model.compile(loss="mean_squared_error", optimizer="adam")
 
 # fit the model
-model.fit(seqstrain, readstrain, batch_size=int(sys.argv[2]), epochs=int(sys.argv[3]), validation_split=0.05)
+model.fit(seqstrain, tempstrain, batch_size=int(sys.argv[2]), epochs=int(sys.argv[3]), validation_split=0.05)
 
 #prediction 
 predicted = model.predict(seqstest)
 model.save(str(sys.argv[4]))
 
+numpts = int(len(seqs)*float(sys.argv[6]))
 ##error
-error = [abs(predicted[i]-readstest[i])*100 for i in range(int(sys.argv[6]))]
+error = [abs(predicted[i]-tempstest[i])*100/tempstest[i] for i in range(numpts)]
 print("avg errror per prediction:")
-print(sum(error)/int(sys.argv[6]))
+print(sum(error)/numpts)
 #plotting
 fig = plt.figure()
 
+if numpts > 150: #graphs are unreadable if it is too big
+    numpts = 150
+
 ax1 = fig.add_subplot(121)
-ax1.plot(predicted[1:int(sys.argv[6])],'--')
-ax1.plot(readstest[1:int(sys.argv[6])],'--')
+ax1.plot(predicted[1:numpts],'--')
+ax1.plot(tempstest[1:numpts],'--')
 ax1.legend(["predictions", "results"])
 
 ax2 = fig.add_subplot(122)
-ax2.plot(error[1:int(sys.argv[6])],'--')
+ax2.plot(error[1:numpts],'--')
 ax2.legend(["predictions-results"])
-
-plt.show()
 plt.savefig(str(sys.argv[5]))
+plt.show()
